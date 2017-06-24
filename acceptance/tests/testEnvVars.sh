3e28a0c4ca093ee780306b35a2597bce10e58881
Barry Allwood <barry.allwood@bjss.com>
2017-06-24 22:31:30 +0100

    Run env var tests on all osx & linux builds

--- acceptance/tests/testEnvVars.sh
+++ acceptance/tests/testEnvVars.sh
@@ -1,28 +1,22 @@
 #!/usr/bin/env bash
 
-# only test env vars on latest node build
-if [ "$TRAVIS_OS_NAME" == "linux" ] && [ "$TRAVIS_NODE_VERSION" == "node" ];
-then
+export npm_config_sauceconnect_cdnurl="http://localhost:8080/downloads"
 
-    export npm_config_sauceconnect_cdnurl="http://localhost:8080/downloads"
+# test
+./acceptance/tests/testCdn.sh
 
-    # test
-    ./acceptance/tests/testCdn.sh
+unset npm_config_sauceconnect_cdnurl
 
-    unset npm_config_sauceconnect_cdnurl
+export sauceconnect_cdnurl="http://localhost:8080/downloads"
 
-    export sauceconnect_cdnurl="http://localhost:8080/downloads"
+# test
+./acceptance/tests/testCdn.sh
 
-    # test
-    ./acceptance/tests/testCdn.sh
+unset sauceconnect_cdnurl
 
-    unset sauceconnect_cdnurl
+export SAUCECONNECT_CDNURL="http://localhost:8080/downloads"
 
-    export SAUCECONNECT_CDNURL="http://localhost:8080/downloads"
+# test
+./acceptance/tests/testCdn.sh
 
-    # test
-    ./acceptance/tests/testCdn.sh
-
-    unset SAUCECONNECT_CDNURL
-
-fi
+unset SAUCECONNECT_CDNURL
