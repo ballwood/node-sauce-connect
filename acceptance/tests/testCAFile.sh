7e234d2b9178a4e843e04225e85f23988e29bdca
Barry Allwood <ballwood@users.noreply.github.com>
2017-08-30 21:59:02 +0100

    Added ca test Added unit tests for httpsRequest & more for installer.js

--- acceptance/tests/testCAFile.sh
+++ acceptance/tests/testCAFile.sh
@@ -25,12 +25,12 @@ ps -p $SSL_PROXY_PID > /dev/null
 SSL_PROXY_TASK_RUNNING=$?
 
 # check if the process is still running by examining the exit code of ps -p
-STRICT_SSL_TEST_RESULT=1
+CA_FILE_TEST_RESULT=1
 
 if [ $SSL_PROXY_TASK_RUNNING -eq 1 ]; then
   # not running, so has been hit.
   echo "SSL proxy finished, test passed"
-  STRICT_SSL_TEST_RESULT=0
+  CA_FILE_TEST_RESULT=0
 else
   echo "SSL proxy not finished, test failed"
   kill $SSL_PROXY_PID
@@ -39,4 +39,4 @@ fi
 unset npm_config_cafile
 unset SAUCECONNECT_CDNURL
 
-exit $STRICT_SSL_TEST_RESULT
+exit $CA_FILE_TEST_RESULT
---
+++ tests/httpsRequest.test.js
@@ -0,0 +1,148 @@
+var httpsRequest = require('../lib/httpsRequest');
+var request = require('request');
+var chai = require('chai');
+var expect = chai.expect;
+var sinon = require('sinon');
+var sinonChai = require('sinon-chai');
+var fs = require('fs');
+var path = require('path');
+chai.use(sinonChai);
+
+describe('httpsRequest', function () {
+
+  describe('create', function () {
+
+    var crtFile = path.resolve(__dirname, '../acceptance/support/keys/ca.crt');
+    var crtFile2 = path.resolve(__dirname, '../acceptance/support/keys/server.crt');
+    var cert1 = fs.readFileSync(crtFile, { encoding: 'ascii' });
+    var cert2 = fs.readFileSync(crtFile2, { encoding: 'ascii' });
+
+    beforeEach(function () {
+      sinon.stub(request, 'defaults');
+    });
+
+    afterEach(function () {
+      request.defaults.restore();
+    });
+
+    it('should set strictSSL to false if strictSSL param = false', function () {
+      httpsRequest.create('false');
+
+      expect(request.defaults).to.have.been.calledWith({
+        strictSSL: false
+      });
+    });
+
+    it('should set strictSSL to false if strictSSL param = FALSE', function () {
+      httpsRequest.create('FALSE');
+
+      expect(request.defaults).to.have.been.calledWith({
+        strictSSL: false
+      });
+    });
+
+    it('should set strictSSL to true if strictSSL param != false', function () {
+      httpsRequest.create('true');
+
+      expect(request.defaults).to.have.been.calledWith({
+        strictSSL: true
+      });
+    });
+
+    it('should set strictSSL to true if strictSSL param is blank', function () {
+      httpsRequest.create('');
+
+      expect(request.defaults).to.have.been.calledWith({
+        strictSSL: true
+      });
+    });
+
+    it('should set strictSSL to true if strictSSL param is undefined', function () {
+      httpsRequest.create();
+
+      expect(request.defaults).to.have.been.calledWith({
+        strictSSL: true
+      });
+    });
+
+    it('should only set strictSSL to true if all params are blank strings', function () {
+      httpsRequest.create('', '', '');
+
+      expect(request.defaults).to.have.been.calledWith({
+        strictSSL: true
+      });
+    });
+
+    it('should set ca and agentOptions.ca if ca param is set', function () {
+      httpsRequest.create('', '', cert1);
+
+      expect(request.defaults).to.have.been.calledWith({
+        ca: cert1,
+        strictSSL: true,
+        agentOptions: {
+          ca: cert1
+        }
+      });
+
+    });
+
+    it('should not modify strictSSL if ca param is set', function () {
+
+      httpsRequest.create('false', '', cert1);
+
+      expect(request.defaults).to.have.been.calledWith({
+        ca: cert1,
+        strictSSL: false,
+        agentOptions: {
+          ca: cert1
+        }
+      });
+
+    });
+
+    it('should load the ca file and set ca and agentOptions.ca if caFile param is set', function () {
+
+      httpsRequest.create('', crtFile);
+
+      expect(request.defaults).to.have.been.calledWith({
+        ca: cert1,
+        strictSSL: true,
+        agentOptions: {
+          ca: cert1
+        }
+      });
+
+    });
+
+
+    it('should not modify strictSSL if caFile param is set', function () {
+
+      httpsRequest.create('false', crtFile);
+
+      expect(request.defaults).to.have.been.calledWith({
+        ca: cert1,
+        strictSSL: false,
+        agentOptions: {
+          ca: cert1
+        }
+      });
+
+    });
+
+    it('should give ca param precedence over caFile param', function () {
+
+      httpsRequest.create('', crtFile, cert2);
+
+      expect(request.defaults).to.have.been.calledWith({
+        ca: cert2,
+        strictSSL: true,
+        agentOptions: {
+          ca: cert2
+        }
+      });
+
+    });
+
+  });
+
+});
