9e35775be5ea498921a50529139c074d68ad5e11
Barry Allwood <ballwood@users.noreply.github.com>
2017-08-30 17:08:16 +0100

    Initial strict-ssl and ca/cafile support

--- acceptance.sh
+++ acceptance.sh
@@ -5,4 +5,6 @@ acceptance/tests/testProxy.sh &&
 acceptance/tests/testNoProxy.sh &&
 acceptance/tests/testAuthProxy.sh &&
 acceptance/tests/testEnvVars.sh &&
+acceptance/tests/testStrictSSL.sh &&
+acceptance/tests/testCertificateAuthority.sh &&
 acceptance/tests/test32.sh
