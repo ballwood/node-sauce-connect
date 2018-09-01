88283ed07fe93967128b2b97183fa5c9b0e29cc1
Barry Allwood <ballwood@users.noreply.github.com>
2018-09-01 14:35:52 +0100

    Exit correctly

--- acceptance/tests/test.sh
+++ acceptance/tests/test.sh
@@ -3,5 +3,13 @@
 MANIFEST_KEY="$TRAVIS_OS_NAME$SC_ARCH"
 
 EXPECTED_SHASUM=$(cat 'manifest.json' | jq -r ".$MANIFEST_KEY.sha1")
+ACTUAL_SHASUM=$(shasum ./bin/sc | cut -b-40)
 
-echo "$EXPECTED_SHASUM *./bin/sc" | shasum -c
+echo "Expected SHASUM: '$EXPECTED_SHASUM'"
+echo "Actual SHASUM: '$ACTUAL_SHASUM'"
+
+if [ "$EXPECTED_SHASUM" == "$ACTUAL_SHASUM" ]; then
+  exit 0;
+fi
+
+exit 1;
